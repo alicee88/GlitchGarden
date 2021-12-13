108c01f4afb39b531e2e660fb309051ffd536b31
alicee88 <59049601+alicee88@users.noreply.github.com>
2021-12-13 08:24:23 +0000

    Add animation events for setting movement speed

--- GlitchGarden/Assets/Scripts/Attacker.cs
+++ GlitchGarden/Assets/Scripts/Attacker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
-    float walkSpeed = 1.0f;
+    float currentSpeed = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -12,10 +12,14 @@ public class Attacker : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
         
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
     }
 }
