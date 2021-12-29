4a4f1c8cd83e5991a79522874748f421addbfda0
alicee88 <59049601+alicee88@users.noreply.github.com>
2021-12-29 11:15:55 +0000

    Place defenders on mouse click inside the grid

---
+++ GlitchGarden/Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defenderPrefab;
+    
+    private void OnMouseDown()
+    {
+        Vector2 clickedPos = GetSquareClicked();
+        SpawnDefender(clickedPos);
+        
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnDefender(Vector2 defenderSpawnPos)
+    {
+        GameObject newDefender = Instantiate(defenderPrefab, defenderSpawnPos, transform.rotation) as GameObject;
+    }
+}
