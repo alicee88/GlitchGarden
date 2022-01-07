f055debea5af365b7d740c6f5cd836dfb76ae2a1
alicee88 <59049601+alicee88@users.noreply.github.com>
2022-01-07 13:08:32 +0000

    Add slider to show time remaining

---
+++ GlitchGarden/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [SerializeField] float levelTime = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+
+        bool timerFinished = (Time.timeSinceLevelLoad > levelTime);
+
+        if(timerFinished)
+        {
+            Debug.Log("TIMER FINISHED!");
+        }
+    }
+}
