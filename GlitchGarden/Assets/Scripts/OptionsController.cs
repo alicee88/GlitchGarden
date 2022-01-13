71948e077e7ac2054b5caa432aa9195d8f1c4a85
alicee88 <59049601+alicee88@users.noreply.github.com>
2022-01-13 07:44:10 +0000

    Add game music and allow player to set volume

---
+++ GlitchGarden/Assets/Scripts/OptionsController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] Slider difficultySlider;
+    float defaultVolume = 0.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if(musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+    }
+
+    public void SaveAndExitToMenu()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadStartScene();
+    }
+
+    public void RestoreDefaults()
+    {
+        volumeSlider.value = defaultVolume;        
+    }
+}
---
+++ GlitchGarden/Assets/Scripts/PlayerPrefsController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsController : MonoBehaviour
+{
+    const string MASTER_VOLUME_KEY = "master volume";
+    const string DIFFICULTY_KEY = "difficulty";
+
+    const float MAX_VOLUME = 1f;
+    const float MIN_VOLUME = 0;
+
+    public static void SetMasterVolume(float volume)
+    {
+        if(volume <= MAX_VOLUME && volume >= MIN_VOLUME)
+        {
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+
+        }
+    }
+
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+
+}
